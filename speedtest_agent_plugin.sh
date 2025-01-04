d6e2002c53cceb5df2f5a619fc866d985ebab51b
Thalion <8501714+thalion0@users.noreply.github.com>
2025-01-04 10:32:47 +0100

    Update speedtest_agent_plugin.sh
    Fixed issues for using current speediest release

--- speedtest_agent_plugin.sh
+++ speedtest_agent_plugin.sh
@@ -19,48 +19,69 @@ if [ "$start_speedtest" = true ]; then
     speedtest --json > $data_filepath
 fi
 
-###Json Example:
-#{
-#  "bytes_received": xxxxx,
-#  "bytes_sent": xxxxxx,
-#  "client": {
-#    "country": "xx",
-#    "ip": "xx.xx.xx.xx",
-#    "isp": "xxxx",
-#    "ispdlavg": "x",
-#    "isprating": "x.x",
-#    "ispulavg": "x",
-#    "lat": "xx.xxx",
-#    "loggedin": "x",
-#    "lon": "x.xxx",
-#    "rating": "x"
+###Json Example:{
+## DATE: 2025-01-04
+#  "type": "result",
+#  "timestamp": "2025-01-04T09:13:13Z",
+#  "ping": {
+#    "jitter": 0.094,
+#    "latency": 9.048,
+#    "low": 8.971,
+#    "high": 9.188
 #  },
-#  "download": xxxxxx.xxx,
-#  "ping": x.xxx,
+#  "download": {
+#    "bandwidth": 8411838,
+#    "bytes": 89986848,
+#    "elapsed": 10710,
+#    "latency": {
+#      "iqm": 9.675,
+#      "low": 9.026,
+#      "high": 259.704,
+#      "jitter": 15.531
+#    }
+#  },
+#  "upload": {
+#    "bandwidth": 2521174,
+#    "bytes": 22452444,
+#    "elapsed": 8811,
+#    "latency": {
+#      "iqm": 59.42,
+#      "low": 8.958,
+#      "high": 450.399,
+#      "jitter": 47.062
+#    }
+#  },
+#  "packetLoss": 0.9345794392523364,
+#  "isp": "Mitteldeutsche Gesellschaft fuer Kommunikation mbH",
+#  "interface": {
+#    "internalIp": "XXX.XXX.XXX.XXX",
+#    "name": "eth0",
+#    "macAddr": "00:00:00:00:00:00",
+#    "isVpn": false,
+#    "externalIp": "XXX.XXX.XXX.XXX"
+#  },
 #  "server": {
-#    "cc": "xx",
-#    "country": "xxxxx",
-#    "d": xx.xxxxxxx,
-#    "host": "url:port",
-#    "id": "xxxx",
-#    "lat": "xx.xxxx",
-#    "latency": x.xxx,
-#    "lon": "x.xxx",
-#    "name": "xxxxx",
-#    "sponsor": "xxxxx",
-#    "url": "http://xxxxxx:port/xxxx"
+#    "id": 36011,
+#    "host": "speedtest2.mdcc.de",
+#    "port": 8080,
+#    "name": "MDCC Magdeburg-City-Com GmbH",
+#    "location": "Magdeburg",
+#    "country": "Germany",
+#    "ip": "213.211.193.38"
 #  },
-#  "share": null,
-#  "timestamp": "yyyy-mm-ddTHH:MM:SS.SSSSST",
-#  "upload": xxxxxx.xxx
+#  "result": {
+#    "id": "3e21a165-521c-13394-a24d-a15e33a3f643",
+#    "url": "https://www.speedtest.net/result/c/3e21a165-521c-13394-a24d-a15e33a3f643",
+#    "persisted": true
+#  }
 #}
 
 # For simplicity, all variables were set in rows as follows
-download_mbps=$(calc -d print $(jq -c '.download' $data_filepath) / 1024 / 1024)
-upload_mbps=$(calc -d print $(jq -c '.upload' $data_filepath) / 1024 / 1024)
+download_mbps=$(calc -d print $(jq -c '.download.bytes' $data_filepath) / 1024 / 1024)
+upload_mbps=$(calc -d print $(jq -c '.upload.bytes' $data_filepath) / 1024 / 1024)
 
-latency_average=$(jq -c '.ping' $data_filepath)
-local_public_ip=$(jq -c '.client.ip' $data_filepath)
+latency_average=$(jq -c '.ping.latency' $data_filepath)
+local_public_ip=$(jq -c '.interface.externapIp' $data_filepath)
 remote_server_ip=$(jq -c '.server.host' $data_filepath)
 
 
